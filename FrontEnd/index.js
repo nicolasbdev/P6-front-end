ea9e600e75cb0cdcffd953eb2752c59b8b5e6d59
nicolasbdev <nicolas.behaegel1@gmail.com>
2025-03-06 15:43:30 +0100

    avancement sur le fichier index.js

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,3 +1,19 @@
+const gallery = document.querySelector(".gallery")
 
 
-addEventListener
+//Requêtes HTTP fonction qui sert à récupérer les travaux en ligne sous format json depuis l'API
+
+async function getwork() {
+	const response = await fetch("http://localhost:5678/api/works")
+	return await response.json()
+}
+
+//Fonction suivante cherche à afficher les travaux de façon dynamique avec une boucle for reach
+
+async function displayWorks() {
+	const works = await getwork()
+	gallery.innerHTML = ""
+	works.forEach((work)=>{
+		createworks(work)
+	})
+}
