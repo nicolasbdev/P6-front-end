a2b6fa0dab28ecf2756f1effcb75802675d9cecf
nicolasbdev <nicolas.behaegel1@gmail.com>
2025-04-17 12:56:25 +0200

    update login html JS / css

---
+++ FrontEnd/login.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form")
+const mail = document.querySelector("form #email")
+const password = document.querySelector("form #password")
+const errorMessage = document.querySelector(".error_message")
+
+form.addEventListener("submit",(event)=>{
+	event.preventDefault()
+	login()
+})
+
+async function login() {
+	if(!validateUser))
+}
